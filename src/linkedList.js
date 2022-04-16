29d772fbba5038a45e73fec29e07c00119c8b73e
Ziya Mukhtarov <ziya.mukhtarov@ug.bilkent.edu.tr>
2022-04-16 17:20:22 +0300

    Add linked list implementation.

---
+++ src/linkedList.js
@@ -0,0 +1,128 @@
+export default class LinkedList {
+    constructor() {
+        /** @var LinkedListNode */
+        this.head = null;
+
+        /** @var LinkedListNode */
+        this.tail = null;
+    }
+
+    /**
+     * @param {*} value
+     * @return {LinkedList}
+     */
+    prepend(value) {
+        const newNode = new LinkedListNode(value, this.head);
+        this.head = newNode;
+
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+
+        return this;
+    }
+
+    /**
+     * @param {*} value
+     * @return {LinkedList}
+     */
+    append(value) {
+        const newNode = new LinkedListNode(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+        return this;
+    }
+
+    /**
+     * @param {*} value
+     * @return {LinkedListNode}
+     */
+    find(value) {
+        if (!this.head) {
+            return null;
+        }
+
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (value !== undefined && currentNode.value === value) {
+                return currentNode;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * @return {LinkedListNode}
+     */
+    deleteHead() {
+        if (!this.head) {
+            return null;
+        }
+
+        const deletedHead = this.head;
+
+        if (this.head.next) {
+            this.head = this.head.next;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return deletedHead;
+    }
+
+    /**
+     * @param {*[]} values
+     * @return {LinkedList}
+     */
+    fromArray(values) {
+        values.forEach((value) => this.append(value));
+        return this;
+    }
+
+    /**
+     * @return {LinkedListNode[]}
+     */
+    toArray() {
+        const nodes = [];
+
+        let currentNode = this.head;
+        while (currentNode) {
+            nodes.push(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        return nodes;
+    }
+
+    /**
+     * @param {function} [callback]
+     * @return {string}
+     */
+    toString(callback) {
+        return this.toArray().map((node) => node.toString(callback)).toString();
+    }
+}
+
+class LinkedListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+
+    toString(callback) {
+        return callback ? callback(this.value) : `${this.value}`;
+    }
+}
